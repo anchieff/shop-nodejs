f061deabe3aad7352a979899e8cb7b6536478a81
anchieff <info@annanekrasova.ru>
2021-11-01 06:46:32 +0400

    feat: start project

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(express.urlencoded({extended: false}));
+
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h2>Page not found</h2>')
+})
+
+app.listen(3000);
