4913de911b936caf4325b8ae6827effb2875b286
anchieff <info@annanekrasova.ru>
2021-11-11 06:39:44 +0400

    feat: add controllers

--- app.js
+++ app.js
@@ -1,32 +1,21 @@
 const express = require('express');
 const path = require('path')
-// const expressHbs = require('express-handlebars')
-
 const app = express();
+const errorController = require('./controllers/error')
 
 app.set('view engine', 'ejs')
-
-// app.engine('hbs', expressHbs({
-//     layoutsDir: 'views/layouts/',
-//     defaultLayout: 'main-layout',
-//     extname: 'hbs'
-// }))
-// app.set('view engine', 'hbs')
-// app.set('view engine', 'pug')
 app.set('views', 'views')
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page not Found'})
-})
+app.use(errorController.get404)
 
 app.listen(3000);
